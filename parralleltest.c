a39f3cb9a48a5346f44f318ff2b504a6a0b8a9a4
zach meade <meadez@tcd.ie>
2020-04-16 12:23:07 +0100

    trying to parr

--- parralleltest.c
+++ parralleltest.c
@@ -415,23 +415,22 @@ void team_conv_sparse(float ** * image, struct sparse_matrix ** * kernels,
 
    int h, w, x, y, c, m, index,start,end;
    float value,outputSaver;
-
+   float *imageRef; struct sparse_matrix * kernel;
    DEBUGGING(fprintf(stderr, "w=%d, h=%d, c=%d\n", w, h, c));
 
    // now compute multichannel, multikernel convolution
    int imgSize = height * width;
    int kernelSize = kernel_order * kernel_order;
-   #pragma omp parallel for if (nkernels > 63) private(w, h, m, kernel_order, nkernels, end, channel, value, outputSaver, imageRef, kernel) shared(image, kernels, output) collapse(3) 
+   #pragma omp parallel for if (nkernels > 63) private(w, h, m, kernel_order, nkernels, end, value, outputSaver, imageRef, kernel) shared(image, kernels, output) collapse(2) 
    for (int wh = 0; wh < imgSize; ++wh) {
-      w = wh / width;
-      h = wh % width;
+      
       for (int xy = 0; xy < kernelSize; xy++) {
-
+         w = wh / width;
+         h = wh % width;
          x = xy / kernel_order;
          y = xy % kernel_order;
-
-         float *imageRef = image[w + x][h + y];
-         struct sparse_matrix * kernel = kernels[x][y];
+         imageRef = image[w + x][h + y];
+         kernel = kernels[x][y];
 
          for (m = 0; m < nkernels; m++) {
 
