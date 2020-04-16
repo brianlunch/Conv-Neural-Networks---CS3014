b64f0f42b78653d024d4c261cd854a3016f18d43
zach meade <meadez@tcd.ie>
2020-04-16 12:51:52 +0100

    trying to parr

--- parralleltest.c
+++ parralleltest.c
@@ -421,9 +421,9 @@ void team_conv_sparse(float ** * image, struct sparse_matrix ** * kernels,
    int imgSize = height * width;
    int kernelSize = kernel_order * kernel_order;
    #pragma omp parallel for if (nkernels > 63) private(w, h, m, kernel_order, nkernels, end, value, outputSaver, imageRef, kernel) shared(image, kernels, output) collapse(2) 
-   for (int wh = 0; wh < imgSize; ++wh) {
-      printf("floating is here");
+   for (int wh = 0; wh < imgSize; ++wh) {      
       for (int xy = 0; xy < kernelSize; xy++) {
+        printf("floating is here");
          w = wh / width;
          h = wh % width;
          x = xy / kernel_order;
