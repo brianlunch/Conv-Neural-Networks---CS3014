e8a3b84bb12813c36fab407bad65f35c6c58aa2f
zach meade <meadez@tcd.ie>
2020-04-17 18:35:59 +0100

    retesting

--- parralleltest.c
+++ parralleltest.c
@@ -414,49 +414,45 @@ void team_conv_sparse(float ** * image, struct sparse_matrix ** * kernels,
    int nchannels, int nkernels, int kernel_order) {
    
   
-   int h, w, x, y, c, m, wh, xy, index,start,end;
-   float value,outputSaver;
-   float *imageRef; struct sparse_matrix * kernel;
-   DEBUGGING(fprintf(stderr, "w=%d, h=%d, c=%d\n", w, h, c));
-   // now compute multichannel, multikernel convolution
-   
-   int imgSize = height * width;
-   int kernelSize = kernel_order * kernel_order;
+   int h, w, x, y, c, m, index;
+  float value;
+#pragma omp parallel private(h, w, x, y, c, m, index, value) shared(image, kernels, output)
+    {
+#pragma omp for collapse(3)
+  // initialize the output matrix to zero
+  for ( m = 0; m < nkernels; m++ ) {
+    for ( h = 0; h < height; h++ ) {
+      for ( w = 0; w < width; w++ ) {
+    output[m][h][w] = 0.0;
+      }
+    }
+  }
 
-   #pragma omp parallel for if(nkernels > 63) private(h, w, x, y, c, m, kernelSize, imgSize, kernel_order, nkernels, end, value, outputSaver, imageRef, kernel) shared(image, kernels, output) collapse(1) 
-   for (wh = 0; wh < imgSize; ++wh) {  
-        w = wh / width;
-        h = wh % width;
-        int threadNum = omp_get_thread_num();
-        printf("%d\n", threadNum);   
-      for (xy = 0; xy < kernelSize; xy++) {
-         
-         
-         x = xy / kernel_order;
-         y = xy % kernel_order;
-        
-         imageRef = image[w + x][h + y];
-         kernel = kernels[x][y];
-         #pragma omp critical 
-         for (m = 0; m < nkernels; m++) {
-            
-            //instead of constantly accessing output [m][h][w] save it to a variable and add at the end
-            outputSaver = output[m][h][w];
-			      end = kernel -> kernel_starts[m + 1]; 
-			
-            for (index = kernel -> kernel_starts[m]; index < end; index++) {
+  DEBUGGING(fprintf(stderr, "w=%d, h=%d, c=%d\n", w, h, c));
 
-              outputSaver += (imageRef[kernel -> channel_numbers[index]]) * (kernel -> values[index]);
-            }
-            
-            output[m][h][w] =outputSaver;
-            //output[m][h][w] += outputSaver;
+  // now compute multichannel, multikernel convolution
 
-         } //
-         
-      } //
-   } //
-   
+#pragma omp for collapse(4)
+  for ( w = 0; w < width; w++ ) {
+    for ( h = 0; h < height; h++ ) {
+      for ( x = 0; x < kernel_order; x++) {
+    for ( y = 0; y < kernel_order; y++ ) {
+      struct sparse_matrix * kernel = kernels[x][y];
+      for ( m = 0; m < nkernels; m++ ) {
+        for ( index = kernel->kernel_starts[m]; index < kernel->kernel_starts[m+1]; index++ ) {
+          int this_c = kernel->channel_numbers[index];
+
+          assert( (this_c >= 0) && (this_c < nchannels) );
+          value = kernel->values[index];
+
+          output[m][h][w] += image[w+x][h+y][this_c] * value;
+        }
+      } // m
+    } // y
+      } // x
+    } // h
+  }// w
+}
 } //
 
 
